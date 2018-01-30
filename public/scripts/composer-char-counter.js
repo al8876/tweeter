8d73ee2a0b82e054c2772a0ae7473f498c5cb26e
al8876 <al8876@gmail.com>
2018-01-30 22:26:37 +0000

    Clear comments on JS code added for readability

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
   $('.new-tweet form textarea').on('keyup', function() {
     let input = $(this).val();
     let charLeft = (140 - input.length);
+
+    // Target the counter class in index.html
     const counter = $(this).parent().children('.counter');
+
+    //If input length is over 140 character, counter colour turns red
     if (input.length > 140) {
       $(counter).css('color', 'red');
     }
