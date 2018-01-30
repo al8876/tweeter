3bd9cafda9381b52a90a89bbe2b55b0ba9d65549
al8876 <al8876@gmail.com>
2018-01-30 22:47:44 +0000

    Changed the tweet max character count to a variable outside the function - JS best practice

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,17 @@
+const maxChar = 140;
+
 $(document).ready(function() {
   $('.new-tweet form textarea').on('keyup', function() {
     let input = $(this).val();
-    let charLeft = (140 - input.length);
+    let charLeft = (maxChar - input.length);
 
     // Target the counter class in index.html
     const counter = $(this).parent().children('.counter');
 
-    //If input length is over 140 character, counter colour turns red
-    if (input.length > 140) {
+    // If input length is over 140 character, counter colour turns red
+    if (input.length > maxChar) {
       $(counter).css('color', 'red');
     }
     counter.text(charLeft);
   });
 });
-
