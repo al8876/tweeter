c66c86e2df4c2e78c34dfd2d75320007ed422538
al8876 <al8876@gmail.com>
2018-01-31 23:29:56 +0000

    Tweets show on page using callback function loadAndRenderTweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,61 +1,65 @@
-// Display existing tweets from database 
+$(document).ready(function(){
 
-function renderTweets(tweets) {
-  tweets.forEach(function(tweet) {
-    $('#tweetFeed').prepend(createTweetElement(tweet));
-  });
-};
+  // Display existing tweets from database 
 
-// Parse tweet database from objects into HTML elements
-
-function createTweetElement(tweet) {
-  let $tweet = $("<article>").addClass("indiTweet");
-  let $header = $("<header>");
-  let $avatars = $("<img>").attr("src", tweet.user.avatars.small);
-  let $name = $("<h2>").text(tweet.user.name);
-  let $handle = $("<p>").text(tweet.user.handle);
-  let $content = $('<p>').text(tweet.content.text); 
-  let $footer = $('<footer>');
-  let $created = $('<p>').text(tweet.created_at);
-  let $heart = $('<i>').addClass("fa fa-heart").attr("aria-hidden", "true");
-  let $retweet = $('<i>').addClass("fa fa-retweet").attr("aria-hidden", "true");
-  let $flag = $('<i>').addClass("fa fa-flag").attr("aria-hidden", "true");
-  $($footer).append($created, $heart, $retweet, $flag);
-  $($header).append($avatars, $name, $handle);
-  $($tweet).append($header, $content, $footer);
-  return $tweet;
-}
-
-// New Tweet Functions
-
-$('.new-tweet form').on('submit', function(e) {
-  // Prevent the default behaviour of new page refresh
-  e.preventDefault();
-  // Get data of form
-  var data = $('.new-tweet form').serialize();
-  // Submit using ajax
-  console.log(data);
-
-  // Condition for text between char count 0 - 140 
-  if ($('textarea').val() === "") {
-    window.alert("Empty");
-  } else if ($('textarea').val().length > 140) {
-    window.alert('Too many characters');
-  } else {
-    $.post('/tweets', data).done(function(){;
-      $(function loadTweets() {
-        $.ajax({
-          url: '/tweets',
-          method: 'GET',
-          success: function (tweet) {
-            console.log('Success :', tweet);
-            renderTweets(tweet);
-            
-          }
-        });
-      });
+  function renderTweets(tweets) {
+    tweets.forEach(function(tweet) {
+      $('#tweetFeed').prepend(createTweetElement(tweet));
     });
-// Reset the form after submit
-    $(this).trigger('reset');
+  };
+
+  // Parse tweet database from objects into HTML elements
+
+  function createTweetElement(tweet) {
+    let $tweet = $("<article>").addClass("indiTweet");
+    let $header = $("<header>");
+    let $avatars = $("<img>").attr("src", tweet.user.avatars.small);
+    let $name = $("<h2>").text(tweet.user.name);
+    let $handle = $("<p>").text(tweet.user.handle);
+    let $content = $('<p>').text(tweet.content.text); 
+    let $footer = $('<footer>');
+    let $created = $('<p>').text(tweet.created_at);
+    let $heart = $('<i>').addClass("fa fa-heart").attr("aria-hidden", "true");
+    let $retweet = $('<i>').addClass("fa fa-retweet").attr("aria-hidden", "true");
+    let $flag = $('<i>').addClass("fa fa-flag").attr("aria-hidden", "true");
+    $($footer).append($created, $heart, $retweet, $flag);
+    $($header).append($avatars, $name, $handle);
+    $($tweet).append($header, $content, $footer);
+    return $tweet;
   }
-});
+
+  function loadAndRenderTweet() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+    }).done(function (tweet) {
+      console.log('Success :', tweet);
+      renderTweets(tweet);
+    });
+  };
+
+  loadAndRenderTweet();
+  
+  // New Tweet Functions
+
+  $('.new-tweet form').on('submit', function(e) {
+    // Prevent the default behaviour of new page refresh
+    e.preventDefault();
+    // Get data of form
+    var data = $('.new-tweet form').serialize();
+    // Submit using ajax
+    console.log(data);
+
+    // Condition for text between char count 0 - 140 
+    if ($('textarea').val() === "") {
+      window.alert("Empty");
+    } else if ($('textarea').val().length > 140) {
+      window.alert('Too many characters');
+    } else {
+      $.post('/tweets', data).done(loadAndRenderTweet);
+  // Reset the form after submit
+      $(this).trigger('reset');
+    }
+  });
+
+})
