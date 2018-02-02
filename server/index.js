a1769b276a3f85d7f8ecc91a5916253e3bcb099a
al8876 <al8876@gmail.com>
2018-02-02 19:54:51 +0000

    npm package SASS installed

--- server/index.js
+++ server/index.js
@@ -1,19 +1,31 @@
 "use strict";
 
 // Basic express setup:
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+// SASS setup:
+const nodeSassMiddleware = require("node-sass-middleware");
+const path = require("path");
+
+// SASS middleware setup:
+app.use(nodeSassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'compressed'
+}));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// MongoDB set up
+// MongoDB setup:
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
-// MongoDB connection set up with database
+// MongoDB connection set up with database:
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error('Error with data entry');
@@ -24,7 +36,7 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   app.use("/tweets", tweetsRoutes);
 });
 
-// Console log server listening port
+// Console log server listening port:
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
